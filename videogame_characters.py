0e01d5a835846bf23246d476aa2637c86b6019c7
Boyoon Choi <choiboyoon11@gmail.com>
2025-02-24 00:16:54 +0100

    add wwr scraper on main.ipynb

--- videogame_characters.py
+++ videogame_characters.py
@@ -22,11 +22,30 @@ class Team:
 
     def introduce(self):
         self.player_names = [player.name for player in self.players]
-        return f"This is {self.team_name} and members are {self.player_names}!"
+        print(f"This is {self.team_name} and members are {self.player_names}!")
     
     def show_players(self):
         for player in self.players:
             player.introduce()
+
+    # code challenge 1: player를 삭제하는 메소드. name 베이스로 어떻게 object를 찾지??? + 해당 플레이어가 존재하지 않을 경우 에러 처리 생각!
+    def rm_player(self, name):
+        for player in self.players:
+            if player.name == name:
+                print(f"{player.name} successfully removed.")
+                self.players.remove(player)
+                return player
+        # if not returned by this point,
+        print(f"{name} doesn't exist in {self.team_name}.")
+        return -1
+
+    # code challenge 2: 팀의 xp 총합을 보여주는 메소드. xp: experience point
+    def total_xp(self):
+        sum_xp=0
+        for player in self.players:
+            sum_xp += player.xp
+        return sum_xp
+
 
 team_blue = Team("Team Blue")
 team_blue.add_player("Boyoon Choi")
@@ -34,6 +53,5 @@ team_red = Team("Team Red")
 team_red.add_player("Nico")
 team_red.add_player("lynn")
 
-team_red.show_players()
-# print(team_blue.introduce())
-# print(team_red.introduce())
+print(team_red.total_xp())
+print(team_blue.total_xp())
